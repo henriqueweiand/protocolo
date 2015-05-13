df0d4a16794cc4ef400b6aa0033b6a073c55947e
Henrique Weiand <nike_weiand@hotmail.com>
2015-05-13 01:30:12 +0000

    Atualizacao

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     </head>
     <body>
 
-        <form name="formProtocolo" id="formProtocolo" method="post" action="" enctype="multipart/form-data">
+        <form name="formProtocolo" id="formProtocolo" method="GET" action="montaProtocolo.php">
 
             <div class="container-fluid">
 
@@ -33,18 +33,18 @@
                             
                             <div class="form-group col-md-6">
                                 <label for="">Endereço de origem</label>
-                                <input type="text" class="form-control" name="" maxlength="15">
+                                <input type="text" class="form-control" name="origem" maxlength="32" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>">
                             </div>
 
                             <div class="form-group col-md-6">
                                 <label for="">Endereço destino</label>
-                                <input type="text" class="form-control" name="" maxlength="15">
+                                <input type="text" class="form-control" name="destino" maxlength="32" value="<?php echo $_SERVER['REMOTE_ADDR']; ?>">
                             </div>
 
                             <div role="tabpanel">
-                                <ul class="nav nav-tabs" role="tablist">
-                                    <li role="presentation" class="active"><a href="#mensagem" aria-controls="mensagem" role="tab" data-toggle="tab">Mensagem</a></li>
-                                    <li role="presentation"><a href="#arquivo" aria-controls="arquivo" role="tab" data-toggle="tab">Arquivo(s)</a></li>
+                                <ul id="tabs" class="nav nav-tabs" role="tablist">
+                                    <li role="presentation" class="active"><a href="#mensagem" aria-controls="mensagem" role="tab" data-toggle="tab">Mensagem <small>(Enviar)</small></a></li>
+                                    <li role="presentation"><a href="#arquivo" aria-controls="arquivo" role="tab" data-toggle="tab">Arquivo <small>(Receber)</small></a></li>
                                 </ul>
 
                                 <div class="tab-content">
@@ -52,15 +52,52 @@
 
                                         <div class="form-group col-md-12" style="padding-top:10px">
                                             <label for="">Mensagem</label>
-                                            <textarea class="form-control" rows="5"></textarea>
+                                            <textarea class="form-control" rows="5" name="mensagem"></textarea>
                                         </div>
                                         
                                     </div>
                                     <div role="tabpanel" class="tab-pane" id="arquivo">
 
-                                        <div class="form-group col-md-12" style="padding-top:10px">
-                                            <label for="">Arquivo(s)</label>
-                                            <input type="file" class="form-control" name="" disabled>
+                                        <div class="form-group col-md-12">
+                                            <?php 
+                                            $files = scandir('/home/ubuntu/workspace/arquivos');
+                                            unset($files[0]);
+                                            unset($files[1]);
+                                            sort($files);
+                                            
+                                            // < Verifica se já foi enviado arquivos
+                                            if(!empty($files)) {
+                                                
+                                                // < Percorre os arquivos enviados
+                                                foreach($files as $file) {
+                                                    
+                                                    list($data['grupo'], $data['nome'], $data['extensao']) = explode('.', $file);
+                                                    
+                                                    if($data['grupo'] != @$grupo) {
+                                                        $grupo = $data['grupo'];
+                                                        echo '
+                                                        <div class="list-group" style="margin-top:15px; margin-bottom:0px">
+                                                            <a href="#" class="list-group-item active">
+                                                                Arquivos da mensagem
+                                                            </a>
+                                                        ';
+                                                    }
+                                                    
+                                                    echo '<a href="#" class="list-group-item">'.$data['nome'].'.'.$data['extensao'].'</a>';
+                                                    
+                                                    if($data['grupo'] != @$grupo) {
+                                                        echo '</div>';
+                                                    }
+                                                    
+                                                }
+                                                // < Percorre os arquivos enviados
+                                                
+                                            } else {
+                                                // Nao foi enviado nada ainda
+                                            }
+                                            // > Verifica se já foi enviado arquivos
+                                            ?>
+                                            
                                         </div>
 
                                     </div>
@@ -73,9 +110,13 @@
 
                         <div class="panel-footer text-right">
                             <button type="reset" class="btn btn-default">Resetar formulário</button>
-                            <button type="button" class="btn btn-success">Enviar</button>             
+                            <button type="submit" class="btn btn-success" name="btnEnviar">Enviar</button>             
                         </div>
                         <!-- END .panel-footer -->
+                        
+                        <div class="col-md-12 result">
+                            
+                        </div>
 
                     </div>
                     <!-- END .panel -->
@@ -94,5 +135,23 @@
 <script type="text/javascript">
 jQuery(document).ready(function($) {
     
+    $('#tabs a').click(function (e) {
+        divID = $(this).attr("href");
+        
+        if(divID == '#arquivo') {
+            $("#arquivo").find("input").prop("disabled", false);
+            $("#mensagem").find("input, textarea").prop("disabled", true);
+            $("button[name=btnEnviar]").text("Receber");
+            $("#formProtocolo").attr("action", "lerProtocolo.php");
+        } else {
+            $("#arquivo").find("input").prop("disabled", true);
+            $("#mensagem").find("input, textarea").prop("disabled", false);
+            $("button[name=btnEnviar]").text("Enviar");
+            $("#formProtocolo").attr("action", "montaProtocolo.php");
+        }
+        
+        e.preventDefault()
+        $(this).tab('show');
+    })
 });
 </script>
---
+++ montaProtocolo.php
@@ -0,0 +1,78 @@
+<?php
+
+	$file = fopen("/home/ubuntu/workspace/arquivos/1.protocolo.txt", "wb");
+
+	$soh = hexdec("0x01");
+	$syn = hexdec("0x16");
+	$si  = hexdec("0x0f");
+	$so  = hexdec("0x0E");
+	$stx = hexdec("0x02");
+	$etx = hexdec("0x03");
+	$eot = hexdec("0x04");
+	
+	$or  = null;
+	$de  = null;
+	$tm  = 0;
+	$sq  = 0;
+	$chk1 = null;
+	$chk2 = null;
+
+	$origem  	= isset($_GET["origem"])   ? $_GET["origem"]   : "";
+	$destino 	= isset($_GET["destino"])  ? $_GET["destino"]  : "";
+	$mensagem 	= isset($_GET["mensagem"]) ? $_GET["mensagem"] : "teste";
+	$msg 		= $mensagem;
+	
+	if(!filter_var($origem, FILTER_VALIDATE_IP) !== false)
+		exit("A origem nao eh um IP valido.");
+	
+	if(!filter_var($destino, FILTER_VALIDATE_IP) !== false)
+		exit("O destino nao eh um IP valido.");
+	
+	$origem = str_replace(".", "", $origem);
+	$destino = str_replace(".", "", $destino);
+
+	$a_m = str_split($msg, 4096);
+	
+	foreach($a_m as $i=>$a) {
+		
+		$tm = dechex(strlen($a));
+		$sq = dechex($i+1);
+
+		$or = array_map("hexdec", array_reverse(str_split(dechex($origem), 2)));
+		$de = array_map("hexdec", array_reverse(str_split(dechex($destino), 2)));
+		
+		for($i=0;$i<5;$i++) {
+			
+			if(strlen($tm) < 3)
+				$tm = "0".$tm;
+			
+			if(strlen($sq) < 5)
+				$sq = "0".$sq;
+		}
+
+		$z = array_map("hexdec", array_reverse(str_split($tm.$sq, 2)));
+
+		$chk1 = ~($soh^$syn^$si^($or[0]^$or[1]^$or[2]^$or[3])^($de[0]^$de[1]^$de[2]^$de[3])^$so^($z[0]^$z[1]^$z[2]^$z[3])) & 0xff;
+
+		$msg = array_map("ord", str_split($a));
+		
+		$chk2 = $soh+$syn+$si+($or[0]+$or[1]+$or[2]+$or[3])+($de[0]+$de[1]+$de[2]+$de[3])+$so+($z[0]+$z[1]+$z[2]+$z[3])+$chk1+$stx+$eot+$etx;
+		
+		foreach($msg as $b)
+			$chk2+= $b;
+
+		$chk2 = array_map("hexdec", array_reverse(str_split(dechex((~$chk2) & 0xffff), 2)));
+
+		$protocolo = chr($soh).chr($syn).chr($si).implode("", array_map("chr", $or)).implode("", array_map("chr", $de)).chr($so).implode("", array_map("chr", $z)).chr($chk1).chr($stx).implode("", array_map("chr", $msg)).chr($etx).chr($eot).implode("", array_map("chr", $chk2));
+
+		fwrite ($file, $protocolo);
+		
+		$protocolo = implode(" => ", array_map("dechex",array_map("ord", str_split($protocolo))));
+	}
+
+	//echo "mensagem: ".$mensagem;
+	echo 'Recebido com sucesso.';
+
+	fclose($file);
+
+?>
